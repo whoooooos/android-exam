3cd3ed7ebea0f83a5ad0b9eaaee6e107c5e639fd
whoooooos <ivansantiago0408@gmail.com>
2024-06-14 19:59:09 +0800

    Created PersonApp for Hilt

---
+++ app/src/main/java/com/example/androidexam/PersonApp.kt
@@ -0,0 +1,10 @@
+package com.example.androidexam
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Ivan Esguerra on 6/11/2024.
+ **/
+@HiltAndroidApp
+class PersonApp: Application()
